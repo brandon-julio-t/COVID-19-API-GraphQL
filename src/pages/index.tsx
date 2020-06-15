cc9299b103c266cce501bba3bee24977c73067f8
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-06-15 17:31:31 +0700

    Added a query sample

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,78 @@
 import React from "react";
 
 export default () => (
-  <div className="h-screen w-screen flex flex-col justify-center items-center">
-    <div className="text-center">
-      <main>
-        <header>
-          <h1 className="text-6xl font-hairline">COVID-19 API GraphQL</h1>
-          <p className="text-3xl">
-            <cite>A free API for data on the Coronavirus</cite> with GraphQL.
-          </p>
-
-          <a className="underline text-xl" href="/.netlify/functions/graphql">
-            Visit the Playground
-          </a>
-        </header>
-      </main>
-
-      <footer className="mt-32">
-        <p>
-          Data sourced from{" "}
-          <a className="underline" href="https://covid19api.com/" rel="noopener noreferrer" target="_blank">
-            covid19api.com
-          </a>
+  <div className="container mx-auto text-center my-16">
+    <main>
+      <header>
+        <h1 className="text-6xl font-hairline">COVID-19 API GraphQL</h1>
+        <p className="text-3xl">
+          <cite>A free API for data on the Coronavirus</cite> with GraphQL.
         </p>
 
+        <a className="underline text-xl" href="/.netlify/functions/graphql">
+          Visit the Playground
+        </a>
+      </header>
+
+      <section className="my-8">
+        <h2 className="text-3xl font-hairline">GraphQL Query Sample</h2>
+
+        <code className="text-left flex flex-row justify-center border rounded my-5 p-5 select-all">
+          <pre>{querySample}</pre>
+        </code>
+
         <p>
-          <a
-            className="underline"
-            href="https://github.com/brandon-julio-t/COVID-19-API-GraphQL"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Source Code
-          </a>
+          Make a POST request to{" "}
+          <span className="select-all underline">https://covid19api-graphql.netlify.app/.netlify/functions/graphql</span>
         </p>
-      </footer>
-    </div>
+      </section>
+    </main>
+
+    <footer>
+      <p>
+        Data sourced from{" "}
+        <a className="underline" href="https://covid19api.com/" rel="noopener noreferrer" target="_blank">
+          covid19api.com
+        </a>
+      </p>
+
+      <p>
+        <a
+          className="underline"
+          href="https://github.com/brandon-julio-t/COVID-19-API-GraphQL"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Source Code
+        </a>
+      </p>
+    </footer>
   </div>
 );
+
+const querySample = `
+query {
+  Summary {
+    Global {
+      NewConfirmed
+      TotalConfirmed
+      NewDeaths
+      TotalDeaths
+      NewRecovered
+      TotalRecovered
+    }
+    Countries {
+      Country
+      CountryCode
+      Slug
+      NewConfirmed
+      TotalConfirmed
+      NewDeaths
+      TotalDeaths
+      NewRecovered
+      TotalRecovered
+      Date
+    }
+  }
+}
+`.trim();
